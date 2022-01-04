15213269c1f450636efa9754ef29745975f7ad5a
forgivehat <876452137@qq.com>
2022-01-04 12:48:50 +0800

    修改：     kernel/printf.c

--- kernel/printf.c
+++ kernel/printf.c
@@ -18,7 +18,8 @@
 volatile int panicked = 0;
 
 // lock to avoid interleaving concurrent printf's.
-static struct {
+static struct
+{
   struct spinlock lock;
   int locking;
 } pr;
@@ -32,20 +33,21 @@ printint(int xx, int base, int sign)
   int i;
   uint x;
 
-  if(sign && (sign = xx < 0))
+  if (sign && (sign = xx < 0))
     x = -xx;
   else
     x = xx;
 
   i = 0;
-  do {
+  do
+  {
     buf[i++] = digits[x % base];
-  } while((x /= base) != 0);
+  } while ((x /= base) != 0);
 
-  if(sign)
+  if (sign)
     buf[i++] = '-';
 
-  while(--i >= 0)
+  while (--i >= 0)
     consputc(buf[i]);
 }
 
@@ -60,30 +62,32 @@ printptr(uint64 x)
 }
 
 // Print to the console. only understands %d, %x, %p, %s.
-void
-printf(char *fmt, ...)
+void printf(char *fmt, ...)
 {
   va_list ap;
   int i, c, locking;
   char *s;
 
   locking = pr.locking;
-  if(locking)
+  if (locking)
     acquire(&pr.lock);
 
   if (fmt == 0)
     panic("null fmt");
 
   va_start(ap, fmt);
-  for(i = 0; (c = fmt[i] & 0xff) != 0; i++){
-    if(c != '%'){
+  for (i = 0; (c = fmt[i] & 0xff) != 0; i++)
+  {
+    if (c != '%')
+    {
       consputc(c);
       continue;
     }
     c = fmt[++i] & 0xff;
-    if(c == 0)
+    if (c == 0)
       break;
-    switch(c){
+    switch (c)
+    {
     case 'd':
       printint(va_arg(ap, int), 10, 1);
       break;
@@ -94,9 +98,9 @@ printf(char *fmt, ...)
       printptr(va_arg(ap, uint64));
       break;
     case 's':
-      if((s = va_arg(ap, char*)) == 0)
+      if ((s = va_arg(ap, char *)) == 0)
         s = "(null)";
-      for(; *s; s++)
+      for (; *s; s++)
         consputc(*s);
       break;
     case '%':
@@ -110,12 +114,11 @@ printf(char *fmt, ...)
     }
   }
 
-  if(locking)
+  if (locking)
     release(&pr.lock);
 }
 
-void
-panic(char *s)
+void panic(char *s)
 {
   pr.locking = 0;
   printf("panic: ");
@@ -123,29 +126,27 @@ panic(char *s)
   printf("\n");
   backtrace();
   panicked = 1; // freeze uart output from other CPUs
-  for(;;)
+  for (;;)
     ;
 }
 
-void
-printfinit(void)
+void printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
 
-void
-backtrace()
-{ 
-  uint64* fp = (uint64*)r_fp();
+void backtrace()
+{
+  uint64 *fp = (uint64 *)r_fp();
   uint64 up = PGROUNDUP((uint64)fp);
-  uint64* ra;
+  uint64 *ra;
   printf("backtrace:\n");
-  while((uint64)fp != up) {
-  ra = (uint64*)((uint64)fp - 8);
-   fp = (uint64*)((uint64)fp - 16);
-   
+  while ((uint64)fp != up)
+  {
+    ra = (uint64 *)((uint64)fp - 8);
+    fp = (uint64 *)((uint64)fp - 16);
     printf("%p\n", *ra);
-    fp = (uint64*)*fp;
+    fp = (uint64 *)*fp;
   }
 }
