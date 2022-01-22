fa1b457dc414f4f4965401a550ca8f916543594d
forgivehat <876452137@qq.com>
2022-01-22 22:52:20 +0800

    finish syscall lab

--- user/sysinfo.c
+++ user/sysinfo.c
@@ -2,10 +2,10 @@
 #include "kernel/types.h"
 #include "user/user.h"
 #include "kernel/sysinfo.h"
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-     struct sysinfo info;
-     sysinfo(&info);
-     printf("free mem:%d, used process num:%d\n",info.freemem,info.nproc);
-        exit(0);
+    struct sysinfo info;
+    sysinfo(&info);
+    printf("free mem:%d, used process num:%d\n", info.freemem, info.nproc);
+    exit(0);
 }
